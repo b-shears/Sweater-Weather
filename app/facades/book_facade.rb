c4672f0be258471645ae2d4f9bb484bcb1b1a497
Bryan Shears <brshears@gmail.com>
2022-09-26 11:39:07 -0600

    refactor books facade

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -2,7 +2,7 @@ class BookFacade
     def self.create_books(location, quantity)
         book_json = BookService.search_books(location, quantity)
         number_found = book_json[:numFound]
-    
+        
         books = book_json[:docs].map do |book|
             Book.new(book)
         end 
