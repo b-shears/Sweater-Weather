07a67117c7dd3dc01dab4c595f62be027753cb27
Bryan Shears <brshears@gmail.com>
2022-09-27 14:35:46 -0600

    update concerns module to include session_json_response method

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -3,6 +3,9 @@ module Response
         render json: UserSerializer.new(object), status: :created
     end 
 
+    def session_json_response(object, status = :ok)
+        render json: SessionSerializer.new(object), status: status
+    end 
     def error_response(message, status)
         render json: message, status: status
     end
