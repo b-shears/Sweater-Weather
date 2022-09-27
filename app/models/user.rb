f92434359f0f313f5eb1f343d67c392a67ce18ea
Bryan Shears <brshears@gmail.com>
2022-09-26 22:04:34 -0600

    add user_request spec happy path test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord 
     validates :email, uniqueness: true, presence: true
     validates_presence_of :password
-
     has_secure_password
+
+    def api_key
+        password_digest
+    end 
 end 
