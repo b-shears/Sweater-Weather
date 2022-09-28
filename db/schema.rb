1e38f047df8e905f81824c5767d892d16fcc8b10
Bryan Shears <brshears@gmail.com>
2022-09-28 12:24:47 -0600

    add api_key to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_215410) do
+ActiveRecord::Schema.define(version: 2022_09_28_182336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_09_26_215410) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
 end
