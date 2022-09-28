dd59fa9ed7bd377ea781682e9d9d6c1e989f8e37
Bryan Shears <brshears@gmail.com>
2022-09-27 19:11:19 -0600

    add services and services_spec for road trip end point

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,12 +1,23 @@
 class LocationService 
-    def self.conn 
+    def self.conn_location 
         Faraday.new("https://www.mapquestapi.com/geocoding/v1/address") do |faraday|
             faraday.params['key'] = ENV['map_quest_key']
         end 
     end 
 
+    def self.conn_route
+        Faraday.new("https://www.mapquestapi.com/directions/v2/route") do |faraday|
+            faraday.params['key'] = ENV['map_quest_key']
+        end 
+    end 
+
     def self.search_location(location)
-        response = conn.get("?location=#{location}")
+        response = conn_location.get("?location=#{location}")
         json = JSON.parse(response.body, symbolize_names: true) if response.status == 200
     end
+
+    def self.route_directions(from, to)
+        response = conn_route.get("?from=#{from}&to=#{to}")
+        json = JSON.parse(response.body, symbolize_names: true) if response.status == 200
+    end 
 end 
