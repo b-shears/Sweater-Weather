30a65f04c62083a35f6aa0a28acbb22f7b2dd359
Bryan Shears <brshears@gmail.com>
2022-09-28 18:35:36 -0600

    refactor formatting

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RoadtripController < ApplicationController
     def create
-      
          if User.exists?(api_key: params[:api_key])
            
             data = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
@@ -10,15 +9,4 @@ class Api::V1::RoadtripController < ApplicationController
             error_response('User is unauthorized must submit an API key', 401)
          end 
     end 
-
-   # def create 
-   #    user = User.find_by(api_key: params[:api_key])
-
-   #    if user.nil?
-   #        error_response('User is unauthorized must submit an API key', 401)
-   #    else 
-   #       data = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
-   #       road_trip_json_response(data)
-   #    end
-   # end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
     validates_presence_of :password
     has_secure_password
    
-    # before_save :api_key
-    
    before_create do 
         self.api_key = SecureRandom.hex(16)
     end 
