311691c3fc163a562793d314721ebc683a704ce1
Bryan Shears <brshears@gmail.com>
2022-09-28 19:44:41 -0600

    update formatting roadtrip controller

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::RoadtripController < ApplicationController
     def create
          if User.exists?(api_key: params[:api_key])
            
-            data = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
+            roadtrip = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
          
-            road_trip_json_response(data)
+            road_trip_json_response(raodtrip)
          else 
             error_response('User is unauthorized must submit an API key', 401)
          end 
