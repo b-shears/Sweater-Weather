f75835f716d242a868c5111b11aeb55f57086d5d
Bryan Shears <brshears@gmail.com>
2022-09-28 18:31:15 -0600

    change api_key to be stored in users table

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,11 +1,24 @@
 class Api::V1::RoadtripController < ApplicationController
     def create
-      user = User.find_by(api_key: params[:api_key])
-         if user.nil?
-           error_response('User is unauthorized must submit an API key', 401)
-         else 
-            roadtrip = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
+      
+         if User.exists?(api_key: params[:api_key])
+           
+            data = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
+         
             road_trip_json_response(data)
+         else 
+            error_response('User is unauthorized must submit an API key', 401)
          end 
     end 
+
+   # def create 
+   #    user = User.find_by(api_key: params[:api_key])
+
+   #    if user.nil?
+   #        error_response('User is unauthorized must submit an API key', 401)
+   #    else 
+   #       data = RoadtripFacade.create_road_trip(params[:origin], params[:destination])
+   #       road_trip_json_response(data)
+   #    end
+   # end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true
     validates_presence_of :password
     has_secure_password
-
-    before_save :api_key
+   
+    # before_save :api_key
     
-    def api_key
+   before_create do 
         self.api_key = SecureRandom.hex(16)
     end 
 end 
