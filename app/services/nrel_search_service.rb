e1916fdf7a1a026abefef2d470e435b824ae7b5e
Bryan Shears <brshears@gmail.com>
2023-01-26 13:38:07 -0700

    complete nrel services

--- app/services/nrel_search_service.rb
+++ app/services/nrel_search_service.rb
@@ -1,12 +1,14 @@
 class NRELSearchService
-    def self.find_station(location)
-        response = conn.get"api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_key']}&location=#{location}&fuel_type=ELEC&limit=1&state=CO"
+    def self.find_nearest_station(location)
+        response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{location}&fuel_type=ELEC&limit=5")
+        json = JSON.parse(response.body, symbolize_names: true) 
     end 
 
     private 
 
     def self.conn 
-        Faraday.new("https://developer.nrel.gov/") do |faraday|
+        Faraday.new("https://developer.nrel.gov") do |faraday|
             faraday.params['key'] = ENV['nrel_key']
+        end
     end 
 end 
