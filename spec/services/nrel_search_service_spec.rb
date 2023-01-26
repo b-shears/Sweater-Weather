3ddaaf2e4d5c924d913885e110544575894b958f
Bryan Shears <brshears@gmail.com>
2023-01-26 13:37:41 -0700

    update nrel_search_service_spec

--- spec/services/nrel_search_service_spec.rb
+++ spec/services/nrel_search_service_spec.rb
@@ -1,8 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe NRELSearchService do 
+RSpec.describe NRELSearchService, :vcr do 
 
+    context "#find_nearest_station(location)" do 
+        it "can return the nearest electric charging station to your searched location" do 
+            json = NRELSearchService.find_nearest_station(80538)
+          
+            expect(json).to be_a(Hash)
+            expect(json).to have_key(:latitude)
+            expect(json[:latitude]).to eq(40.44561)
 
+            expect(json).to have_key(:longitude)
+            expect(json[:longitude]).to eq(-105.07816)
 
+            expect(json).to include(:location_country, :fuel_stations)
 
+            expect(json).to have_key(:location_country)
+            expect(json[:location_country]).to eq("US")
+
+            expect(json).to have_key(:fuel_stations)
+            # binding.pry
+            json[:fuel_stations].each do |fuel_station|
+                expect(fuel_station).to have_key(:access_code)
+                expect(fuel_station).to have_key(:access_days_time)
+                expect(fuel_station).to have_key(:fuel_type_code)
+                expect(fuel_station).to have_key(:groups_with_access_code)
+                expect(fuel_station).to have_key(:city)
+                expect(fuel_station).to have_key(:state)
+                expect(fuel_station).to have_key(:street_address)
+                expect(fuel_station).to have_key(:zip)
+                expect(fuel_station).to have_key(:station_name)
+                expect(fuel_station).to have_key(:latitude)
+                expect(fuel_station).to have_key(:longitude)
+                expect(fuel_station).to have_key(:distance)
+            end 
+        end 
+    end
 end 
