ba0ea3c56319fcce23d4a3ba303f64f9d838f4af
Bryan Shears <brshears@gmail.com>
2023-01-26 21:17:44 -0700

    complete nrel_nearest_station_poro

--- spec/poros/nrel_spec.rb
+++ spec/poros/nrel_spec.rb
@@ -13,8 +13,12 @@ RSpec.describe NREL do
         expect(nearest_station.latitude).to eq(40.44561)
         expect(nearest_station.longitude).to eq(-105.07816)
         expect(nearest_station.location_country).to eq("US")
-
+      
+        expect(nearest_station.address).to eq("375 west 37th street Loveland, CO 80538")
+        expect(nearest_station.fuel_type).to eq("ELEC")
+        expect(nearest_station.public_station).to eq("public")
+        expect(nearest_station.hours_available).to eq("24 hours daily")
+        expect(nearest_station.station_name).to eq("LOVELAND HA2018 LOVELAND HA2018")
+        expect(nearest_station.distance).to eq(0.99441)
     end 
-
-
 end 
