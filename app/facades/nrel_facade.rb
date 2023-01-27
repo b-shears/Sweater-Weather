8214c59e632a2bc1851c946c731a95573670089b
Bryan Shears <brshears@gmail.com>
2023-01-26 21:46:13 -0700

    add nrel_facade_spec

--- app/facades/nrel_facade.rb
+++ app/facades/nrel_facade.rb
@@ -1,4 +1,7 @@
 class NRELFacade 
-
+    def self.create_nearest_station(nearest_station)
+        nrel_json = NRELSearchService.find_nearest_station(nearest_station)
+        nearest_station = NREL.new(nrel_json)
+    end
 
 end 
