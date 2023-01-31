6d0c691a095d39638c2bff22206be6eb30042fe3
Bryan Shears <brshears@gmail.com>
2023-01-31 14:46:06 -0700

    update  nearest_station facade

--- app/facades/nearest_station_facade.rb
+++ app/facades/nearest_station_facade.rb
@@ -1,8 +1,6 @@
 class NearestStationFacade
-    
-    def self.create_nearest_station(nearest_station)
-        nrel_json = NRELSearchService.find_nearest_station(nearest_station)
+    def self.create_nearest_station(location)
+        nrel_json = NRELSearchService.find_nearest_station(location)
         nearest_station = NearestStation.new(nrel_json)
     end
-
 end 
